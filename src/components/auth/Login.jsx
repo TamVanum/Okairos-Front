e2be108a4c23cb664c66c1be2aaf708b6518656e
TamVanum <gaston.cath14@gmail.com>
2024-07-20 22:37:36 -0400

    feat añadi tailwind al proyecto

--- src/components/auth/Login.jsx
+++ src/components/auth/Login.jsx
@@ -49,7 +49,21 @@ function Login() {
                 </div>
                 <button type="submit">Iniciar sesión</button>
             </form>
+            <div className="grid grid-cols-10 gap-2">
+                <div className="bg-sky-50 aspect-square"></div>
+                <div className="bg-sky-100 aspect-square"></div>
+                <div className="bg-sky-200 aspect-square"></div>
+                <div className="bg-sky-300 aspect-square"></div>
+                <div className="bg-sky-400 aspect-square"></div>
+                <div className="bg-sky-500 aspect-square"></div>
+                <div className="bg-sky-600 aspect-square"></div>
+                <div className="bg-sky-700 aspect-square"></div>
+                <div className="bg-sky-800 aspect-square"></div>
+                <div className="bg-sky-900 aspect-square"></div>
+            </div>
         </div>
+
+
     );
 }
 
